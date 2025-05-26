7739bd24a0ea20b298d662ea7d9ecdde4b14002a
Lucas Christian <lucaschristian10@gmail.com>
2025-05-26 12:56:58 -0300

    fix: Adicionar import CrossOrigin no UserController

--- src/main/java/com/lucasreis/palpitef1backend/controllers/UserController.java
+++ src/main/java/com/lucasreis/palpitef1backend/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.lucasreis.palpitef1backend.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
