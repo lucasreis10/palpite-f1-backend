b41aba452fcd005c09e161c5a3e563b55a5480a0
Lucas Christian <lucaschristian10@gmail.com>
2025-05-26 12:22:07 -0300

    resolver erro de cors

--- src/main/java/com/lucasreis/palpitef1backend/infra/security/CorsConfiguration.java
+++ src/main/java/com/lucasreis/palpitef1backend/infra/security/CorsConfiguration.java
@@ -19,7 +19,8 @@ public class CorsConfiguration {
             "http://localhost:3000",
             "http://127.0.0.1:3000",
             "https://localhost:3000",
-            "https://127.0.0.1:3000"
+            "https://127.0.0.1:3000",
+            "https://palpite-f1-frontend-production.up.railway.app"
         ));
         
         // Permitir todos os métodos HTTP
